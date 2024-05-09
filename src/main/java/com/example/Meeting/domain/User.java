697c9f2b2de320ec70a00eb41b7db2769497712c
bangyewon <byewon1111@gmail.com>
2024-05-09 23:15:32 +0900

    [feat] User 엔티티 추가

--- src/main/java/com/example/Meeting/domain/User.java
+++ src/main/java/com/example/Meeting/domain/User.java
@@ -1,9 +1,27 @@
 package com.example.Meeting.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 
+@Entity
 @Getter
-@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    String username;
+    String passward;
+    String email;
+
+    @Column(nullable = true)
+    String profileImage;
+
+    @Column(nullable = false)
+    String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
 }
