e2510e4dfd6dfbb18b353445d639688d29ae4345
bangyewon <byewon1111@gmail.com>
2024-05-10 15:42:38 +0900

    [refactor] UserRequestDTO 수정

--- src/main/java/com/example/Meeting/Web/UserRequestDTO.java
+++ src/main/java/com/example/Meeting/Web/UserRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.Meeting.Web;
 
+import com.example.Meeting.domain.Gender;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,4 +8,10 @@ import lombok.Setter;
 @Setter
 public class UserRequestDTO {
     private String username;
+    private String passward;
+    private String email;
+    private String phone;
+    private String profileImage;
+    private Gender gender;
+
 }
