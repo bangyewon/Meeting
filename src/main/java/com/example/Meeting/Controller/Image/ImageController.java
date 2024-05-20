541dca38ce72b14cd138691ce22521d062251cd6
bangyewon <byewon1111@gmail.com>
2024-05-20 16:14:03 +0900

    [feat] profleimage 엔티티 작성

---
+++ src/main/java/com/example/Meeting/Controller/Image/ImageController.java
@@ -0,0 +1,11 @@
+package com.example.Meeting.Controller.Image;
+
+import com.example.Meeting.domain.Image.Image;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class ImageController {
+
+}
