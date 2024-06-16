dbd0cad22dc1578e7a1d9231ca4404e2ed7e36fa
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-06-16 23:52:52 +0900

    [refactor] 채팅방 전체조회 기능 수정

--- src/main/java/com/example/Meeting/Service/ChatService.java
+++ src/main/java/com/example/Meeting/Service/ChatService.java
@@ -1,9 +1,11 @@
 package com.example.Meeting.Service;
 
+import com.example.Meeting.Repository.ChatRoomRepository;
 import com.example.Meeting.domain.chat.ChatRoomVO;
 import com.example.Meeting.dto.ChatRequestDTO;
 import com.example.Meeting.dto.ChatResponseDTO;
 import com.example.Meeting.exception.DsException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -12,38 +14,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
-    //사용자 list
-    private List<String> userList = new ArrayList<>();
-    //채팅방 list
-    private List<ChatRoomVO> roomList = new ArrayList<>();
 
-    // 방 찾기
-    public ChatRoomVO getTargetRoom(ChatRoomVO chatRoomVO) {
-        for(int i = 0; i < roomList.size(); i++) {
-            if(chatRoomVO.getRoomId() == roomList.get(i).getRoomId()){
-                return roomList.get(i);
-            }
-        }
-        throw new DsException("대화방을 찾지 못했습니다.", HttpStatus.NOT_FOUND);
-    }
+    private final ChatRoomRepository chatRoomRepository;
+    final List<ChatResponseDTO.findChatRoomDTO> roomList = new ArrayList<>();
+
     // 채팅방 목록 조회
-    public List<ChatRoomVO> selectRoomList() {
+    public List<ChatResponseDTO.findChatRoomDTO> findAllChatRoom() {
+        List<ChatRoomVO> chatRoomList = chatRoomRepository.findAll();
+        roomList.clear(); // 기존 목록을 초기화
+        for (ChatRoomVO chatRoomVO : chatRoomList) {
+            roomList.add(new ChatResponseDTO.findChatRoomDTO(chatRoomVO)); // 객체 생성해 저장하기
+        }
         return roomList;
     }
 
     // 채팅방 생성
     public ChatResponseDTO.AddChatRoomDTO addChatRoom(ChatRequestDTO.AddChatRoomDTO addChatRoomDTO) {
+        // 유저 확인하고 생성해야할듯 리팩토링 필요
         ChatRoomVO chatRoomVO = new ChatRoomVO();
         chatRoomVO.setRoomName(addChatRoomDTO.getRoomName());
         chatRoomVO.setRoomId((long) roomList.size() + 1); // 고유 roomId 설정
-        roomList.add(chatRoomVO);
+        chatRoomRepository.save(chatRoomVO);
 
         ChatResponseDTO.AddChatRoomDTO responseDTO = new ChatResponseDTO.AddChatRoomDTO(chatRoomVO);
         responseDTO.setRoomId(chatRoomVO.getRoomId());
         responseDTO.setRoomName(chatRoomVO.getRoomName());
 
         System.out.println("채팅방이 생성됐습니다.");
-        return new ChatResponseDTO.AddChatRoomDTO(chatRoomVO);
+        return responseDTO;
     }
 }
--- src/main/java/com/example/Meeting/domain/chat/ChatRoomVO.java
+++ src/main/java/com/example/Meeting/domain/chat/ChatRoomVO.java
@@ -1,15 +1,18 @@
 package com.example.Meeting.domain.chat;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.example.Meeting.common.BaseEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 @Table(name = "chatroom")
-public class ChatRoomVO {
-    // 키
+public class ChatRoomVO extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Long roomId;
     // 수신자
     private String user1Id;
