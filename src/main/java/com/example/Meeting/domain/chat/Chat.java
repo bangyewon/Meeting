51a8ee5e42f9464ae2bf5d64ecac17ae137c9920
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-06-24 18:17:06 +0900

    [refactor] chat 엔티티 수정

--- src/main/java/com/example/Meeting/domain/chat/Chat.java
+++ src/main/java/com/example/Meeting/domain/chat/Chat.java
@@ -1,7 +1,10 @@
 package com.example.Meeting.domain.chat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import com.example.Meeting.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 
 @Entity
 @Table(name = "chat")
@@ -9,6 +12,20 @@ public class Chat extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "username")
+    private String userName;
+    @Column(name = "msg")
+    private String msg;
+    @Column(name = "created_time")
+    private String createdTime;
     @Column(name = "room_id")
-    private String roomId;
+    private Long roomId;
+
+    @Builder
+    public  Chat(String msg,String userName,Long roomId) {
+        this.msg = msg;
+        this.roomId = roomId;
+        this.userName = userName;
+        this.createdTime = ZonedDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+    }
 }
