95c92f18ed1778410ab182bed568386d39c6f712
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-06-24 18:17:34 +0900

    [feat] 메시지 저장 기능 추가

---
+++ src/main/java/com/example/Meeting/Service/ChatMessageService.java
@@ -0,0 +1,34 @@
+package com.example.Meeting.Service;
+
+import com.example.Meeting.Repository.ChatMessageRepository;
+import com.example.Meeting.Repository.UserRepository;
+import com.example.Meeting.domain.chat.Chat;
+import com.example.Meeting.dto.ChatRequestDTO;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+// 채팅방 메시지 저장
+public class ChatMessageService {
+    private final ChatMessageRepository chatMessageRepository;
+    private final UserRepository userRepository;
+
+    @Transactional // 저장하기
+    public void save(final ChatRequestDTO.ChatMessageDTO chatMessageDTO) {
+        Chat chat = chatMessageDTO.toEntity();
+        chatMessageRepository.save(chat);
+    }
+//    @Transactional
+//    public List<ChatResponseDTO> findRoom(Long roomId) {
+//        List<Chat> chatList = this.chatMessageRepository.findByRoomId(roomId);
+//
+//        List<ChatResponseDTO> dtolist = new ArrayList<>();
+//        for(Chat chat : chatList) {
+//            ChatResponseDTO.ChatRoomDTO chatRoomDTO = new ChatResponseDTO.ChatRoomDTO(chat);
+//            dtolist.add(chatRoomDTO);
+//        }
+//        return dtolist;
+//    }
+}
